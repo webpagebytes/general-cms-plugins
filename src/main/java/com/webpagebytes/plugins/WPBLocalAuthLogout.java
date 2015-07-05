ad580a0a369f8dbe6bfa9f02180a958a536c0dcb
webpagebytes <admin@webpagebytes.com>
2015-07-05 13:37:30 +0300

    add licence header to the files where this was missing

--- src/main/java/com/webpagebytes/plugins/WPBLocalAuthLogout.java
+++ src/main/java/com/webpagebytes/plugins/WPBLocalAuthLogout.java
@@ -1,3 +1,19 @@
+/*
+ *   Copyright 2015 Webpagebytes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package com.webpagebytes.plugins;
 
 import java.io.File;
@@ -12,6 +28,7 @@ import com.webpagebytes.cms.utility.CmsBase64Utility;
 
 public class WPBLocalAuthLogout extends HttpServlet {
 
+private static final long serialVersionUID = 1L;
 private String url_login_page = "";
 public static final String URL_LOGIN_PAGE = "url-login-page"; 
 
